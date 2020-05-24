286e17c1755b90eebed2efd6d94e41287109504b
rakasiwisurya <rakasiwi.surya@gmail.com>
2020-05-24 16:39:13 +0700

    merubah navbar sebelumnya v-toolbar menjadi v-app-bar

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 
     <Navbar />
 
-    <v-content>
+    <v-content class="mx-4 mb-4">
       <router-view></router-view>
     </v-content>
   </v-app>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,8 @@
 <template>
     <nav>
-        <v-toolbar flat app>
-            <v-app-bar-nav-icon class="grey--text" @click="drawer = true"></v-app-bar-nav-icon>
-            <v-toolbar-title class="text-uppercase grey--text">
-                <span class="font-weight-light">Todo</span>
-                <span>App</span>
-            </v-toolbar-title>
 
-            <v-spacer></v-spacer>
-
-            <v-btn text color="grey">
-                <span>Sign Out</span>
-                <v-icon right>exit_to_app</v-icon>
-            </v-btn>
-        </v-toolbar>
-
-        <v-navigation-drawer v-model="drawer" class="info" app temporary dark>
-            <v-btn icon @click="drawer = false">
+        <v-navigation-drawer v-model="drawer" class="info" app dark>
+            <v-btn icon @click="drawer = !drawer">
                 <v-icon>mdi-arrow-left</v-icon>
             </v-btn>
             
@@ -53,6 +39,21 @@
                 </v-list-item-group>
             </v-list>
         </v-navigation-drawer>
+
+        <v-app-bar flat app>
+            <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>App</span>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn text color="grey">
+                <span>Sign Out</span>
+                <v-icon right>exit_to_app</v-icon>
+            </v-btn>
+        </v-app-bar>
     </nav>
 </template>
 
