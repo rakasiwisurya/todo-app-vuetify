72ef650e61ba72f4f553e07b66769c2c5eb796d7
rakasiwisurya <rakasiwi.surya@gmail.com>
2020-06-01 01:51:10 +0700

    menambahkan datepicker dalam form project barul

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -11,6 +11,14 @@
                 <v-form class="px-3">
                     <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
                     <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+
+                    <v-menu :close-on-content-click="false" v-model="menuDatePicker" transition="scale-transition" offset-y>
+                        <template v-slot:activator="{ on }">
+                            <v-text-field v-model="due" label="Due Date" prepend-icon="date_range" v-on="on"></v-text-field>
+                        </template>
+                        <v-date-picker v-model="due" @input="menuDatePicker = false"></v-date-picker>
+                    </v-menu>
+
                     <v-btn text class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
                 </v-form>
             </v-card-text>
@@ -19,16 +27,19 @@
 </template>
 
 <script>
+
 export default {
     data() {
         return {
             title: '',
             content: '',
+            due: null,
+            menuDatePicker: '',
         }
     },
     methods: {
         submit(){
-            console.log(this.title, this.content)
+            console.log(this.title, this.content, this.due)
         }
     },
 }
