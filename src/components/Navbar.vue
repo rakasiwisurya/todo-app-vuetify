2cce52f2f69751c3a4713c8498801fdf11e1d8ce
rakasiwisurya <rakasiwi.surya@gmail.com>
2020-05-31 13:37:11 +0700

    membuat menu button di navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,7 +9,7 @@
             <v-layout column align-center>
                 <v-flex class="mt-5 text-center">
                     <v-avatar size="100">
-                        <img src="/avatar-1.png">
+                        <img src="/img/avatar-1.png">
                     </v-avatar>
                     <div class="white--text subtitle-1 font-weight-bold mt-3">
                         Rakasiwi Surya
@@ -21,6 +21,7 @@
             </v-layout>
 
             <v-divider></v-divider>
+
             <v-list flat dense>
                 <v-list-item-group>
                     <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
@@ -44,6 +45,21 @@
             </v-toolbar-title>
 
             <v-spacer></v-spacer>
+
+            <!-- Dropdown Menu -->
+            <v-menu offset-y>
+                <template v-slot:activator="{ on }">
+                    <v-btn text color="grey" v-on="on">
+                        <v-icon left>expand_more</v-icon>
+                        <span>Menu</span>
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route" >
+                        <v-list-item-title>{{ link.text }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
 
             <v-btn text color="grey">
                 <span>Sign Out</span>
--- src/views/Team.vue
+++ src/views/Team.vue
@@ -9,7 +9,7 @@
           <v-card flat class="text-center ma-3">
             <v-responsive class="pt-4">
               <v-avatar size="100" class="grey lighten-2">
-                <img :src="person.avatar" alt="">
+                <img :src="person.avatar">
               </v-avatar>
             </v-responsive>
             <v-card-text>
@@ -38,11 +38,11 @@ export default {
   data() {
     return {
       team:[
-        {name: 'Rakasiwi Surya', role: 'Frontend Web Developer', avatar: '/avatar-1.png'},
-        {name: 'Erik Prasetya', role: 'Android Developer', avatar: '/avatar-2.png'},
-        {name: 'Rey', role: 'Backend Web Developer', avatar: '/avatar-3.png'},
-        {name: 'Ikbal Aziz', role: 'Network Engineer', avatar: '/avatar-4.png'},
-        {name: 'Robi Habibulloh', role: 'UI/UX', avatar: '/avatar-5.png'},
+        {name: 'Rakasiwi Surya', role: 'Frontend Web Developer', avatar: '/img/avatar-1.png'},
+        {name: 'Erik Prasetya', role: 'Android Developer', avatar: '/img/avatar-2.png'},
+        {name: 'Rey', role: 'Backend Web Developer', avatar: '/img/avatar-3.png'},
+        {name: 'Ikbal Aziz', role: 'Network Engineer', avatar: '/img/avatar-4.png'},
+        {name: 'Robi Habibulloh', role: 'UI/UX', avatar: '/img/avatar-5.png'},
       ]
     }
   },
